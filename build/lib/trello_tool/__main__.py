23cf9897165046c87cc978105f2f4913121ee74f
shiwei.fu <shiwei.fu@thoughtworks.com>
2021-04-18 18:37:55 +0800

    change template
    Signed-off-by: shiwei.fu <shiwei.fu@thoughtworks.com>

--- build/lib/trello_tool/__main__.py
+++ build/lib/trello_tool/__main__.py
@@ -45,7 +45,7 @@ def get_card_from_list(list_of_card_id):
         url,
         params=query
     )
-    return response.text
+    return json.loads(response.text)
 
 
 def create_check_list_for_work_flow(card_id):
@@ -106,9 +106,62 @@ def update_card_desc(card_id, desc):
     return response
 
 
-def create_new_card_for_story(name, url):
-    print(name)
-    new_card = create_card_list(config['stay_stuck_id'], config['template_card_id'], name.split(".")[0])
+def get_card_list_from_broad():
+    url = "https://api.trello.com/1/boards/{0}/lists".format(config['broad_id'])
+
+    query = {
+        'key': config['key'],
+        'token': config['token']
+    }
+
+    response = requests.request(
+        "GET",
+        url,
+        params=query
+    )
+
+    return json.loads(response.text)
+
+
+def get_stay_stuck_id():
+    card_lists = get_card_list_from_broad()
+    stay_stuck_id = ''
+    for card_list in card_lists:
+        if card_list['name'] == config['stay_stuck_name']:
+            stay_stuck_id = card_list['id']
+    if stay_stuck_id == '':
+        print("stay_stuck_name error")
+        sys.exit(1)
+    else:
+        return stay_stuck_id
+
+
+def get_template_card_id():
+    template_card_list_name = config['template_card_list_name']
+    template_card_name = config['template_card_name']
+    card_lists = get_card_list_from_broad()
+    template_card_list_id = ''
+    template_card_id = ''
+    for card_list in card_lists:
+        if card_list['name'] == template_card_list_name:
+            template_card_list_id = card_list['id']
+    if template_card_list_id == '':
+        print("template_card_name error")
+        sys.exit(1)
+    else:
+        cards_in_template_list = get_card_from_list(template_card_list_id)
+        for card in cards_in_template_list:
+            if card['name'] == template_card_name:
+                template_card_id = card['id']
+        if template_card_id == '':
+            print("template_card_name error")
+            sys.exit(1)
+        else:
+            return template_card_id
+
+
+def create_new_card_for_story(name, url, stay_stuck_id, template_card_id):
+    new_card = create_card_list(stay_stuck_id, template_card_id, name.split(".")[0])
     new_card_id = new_card['id']
     work_flow_check_list = create_check_list_for_work_flow(new_card_id)
     work_flow_check_list_id = work_flow_check_list['id']
@@ -140,11 +193,9 @@ def main(args=None):
     read_config()
     file_url = config['file_base_uri'] + sys.argv[1]
     file_name = sys.argv[2] + ".md"
-    create_new_card_for_story(file_name, file_url)
-
-    # Do argument parsing here (eg. with argparse) and anything else
-    # you want your project to do. Return values are exit codes.
-
+    stay_stuck_id = get_stay_stuck_id()
+    template_card_id = get_template_card_id()
+    create_new_card_for_story(file_name, file_url, stay_stuck_id, template_card_id)
 
 if __name__ == "__main__":
     sys.exit(main())
